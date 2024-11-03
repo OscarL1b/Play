2d744a52421ab5b0dc67a56979cb816707cf3377
Oscar <Oscar@DIMA>
2024-11-04 01:28:56 +0300

    Лок

--- Play/Play.cpp
+++ Play/Play.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 void SetSettingConsole();
 vector < Army* > Opponents(vector < string > rareCom, vector < string > epicCom, vector < string > legendaryCom);
+void Menu_Push_Army();
 
 int main()
 {
@@ -15,6 +16,131 @@ int main()
 	vector < string > epicCommander = {"Шарль де Голль", "Константин Рокоссовский", "Дуглас Макартур", "Джордж Джиффард"};
 	vector < string > legendaryCommander = {"Георгий жуков", "Эрих фон Манштейн", "Георг Ханс Райнхардт", "Ханс Гюнтер фон Клюге"};
 	vector < Army* > opponents = Opponents(rareCommander, epicCommander, legendaryCommander);
+	string userNameCountry;
+	Army* userArmy = new Army;
+	bool flag = true;
+	int choiseMenu;
+	vector < string > userCom;
+	static int pointForPushArmy = 100;
+	
+	cout << "---------  ---------" << endl;
+	cout << "Введите название вашей страны: ";
+	cin >> userNameCountry;
+	cout << "Вам в подарок достаётся редкий командир! " << endl;
+	cout << rareCommander[1] << endl;
+
+	userArmy->Fill(userNameCountry, rareCommander[1], 1000, 0, 0, 0);
+
+	cout << "Ваша армия: " << endl;
+	userArmy->Print();
+
+	userCom.push_back(rareCommander[1]);
+
+	while (flag)
+	{
+		cout << "------ Меню ------" << endl;
+		cout << "1. Напасть на страну." << endl
+			<< "2. Увеличить армию." << endl
+			<< "3. Открыть бокс." << endl;
+		cin >> choiseMenu;
+
+		switch (choiseMenu)
+		{
+		case 1:
+
+
+
+		case 2:
+
+			int variantMenu;
+
+			cout << "Ваши поинты добавления армии " << pointForPushArmy << endl;
+
+			Menu_Push_Army();
+			cout << ">>> " << endl;
+			cin >> variantMenu;
+
+			switch (variantMenu)
+			{
+				int quantityPushArmy = 0;
+
+			case 1:
+
+				cout << "Цена 1 поинт за 1 пехотинца." << endl;
+				cout << "Введите сколько хотите добавить пехоты: ";
+				cin >> quantityPushArmy;
+
+				if (quantityPushArmy % 1 <= pointForPushArmy)
+				{
+					userArmy->SetInfantry(quantityPushArmy);
+					pointForPushArmy -= (quantityPushArmy * 1);
+				}
+
+				quantityPushArmy = 0;
+
+				break;
+
+			case 2:
+
+				cout << "Цена 10 поинтов за 1 танк. " << endl;
+				cout << "Введите сколько хотите добавить танков: ";
+				cin >> quantityPushArmy;
+
+				if (quantityPushArmy % 10 <= pointForPushArmy)
+				{
+					userArmy->SetTanks(quantityPushArmy);
+					pointForPushArmy -= (quantityPushArmy * 1);
+				}
+
+				quantityPushArmy = 0;
+
+				break;
+
+			case 3:
+
+				cout << "Цена 3 поинта за 1 снайпера. " << endl;
+				cout << "Введите сколько хотите добавить снайперов: ";
+				cin >> quantityPushArmy;
+
+				if (quantityPushArmy % 3 <= pointForPushArmy)
+				{
+					userArmy->SetSnipers(quantityPushArmy);
+					pointForPushArmy -= (quantityPushArmy * 1);
+				}
+
+				quantityPushArmy = 0;
+
+				break;
+
+			case 4:
+
+				cout << "Цена 5 поинтов за 1 артилерию. " << endl;
+				cout << "Введите сколько хотите добавить артилерии: ";
+				cin >> quantityPushArmy;
+
+				if (quantityPushArmy % 3 <= pointForPushArmy)
+				{
+					userArmy->SetSnipers(quantityPushArmy);
+					pointForPushArmy -= (quantityPushArmy * 1);
+				}
+
+				quantityPushArmy = 0;
+
+				break;
+
+			case 5:
+				continue;
+
+			default:
+
+				cerr << "Такого выбора нет! " << endl;
+				break;
+			}
+
+		default:
+			break;
+		}
+	}
 	
 	return 0;
 }
@@ -31,13 +157,13 @@ vector < Army* > Opponents(vector < string > rareCom, vector < string > epicCom,
 	vector < Army* > opponents;
 
 	opponents.push_back(new Army());
-	opponents.back()->Fill("Вильвия", rareCom[0], 900000, 220, 1000, 4300);
+	opponents.back()->Fill("Вильвия", rareCom[0], 1000, 0, 0, 0);
 	opponents.push_back(new Army());
-	opponents.back()->Fill("Фатумланд", "", 2000000, 700, 1800, 6400);
+	opponents.back()->Fill("Фатумланд", epicCom[1], 2000000, 700, 1800, 6400);
 	opponents.push_back(new Army());
-	opponents.back()->Fill("Рейв", "", 2200000, 1000, 2300, 6500);
+	opponents.back()->Fill("Рейв", epicCom[2], 2200000, 1000, 2300, 6500);
 	opponents.push_back(new Army());
-	opponents.back()->Fill("Ливон", "", 4, 6, 7, 8);
+	opponents.back()->Fill("Ливон", epicCom[1], 4, 6, 7, 8);
 
 	return opponents;
 }
@@ -61,4 +187,15 @@ string Type_Army(string parCom)
 		else if (commanderArtillery[i] == parCom)
 			return "Артилерия";
 	}
+}
+
+void Menu_Push_Army()
+{
+	cout << "Выберите кого хотите добавить:" << endl;
+	cout << "1. Добавить пехоту."
+		<< " 2. Добавить танки."
+		<< "3. Добавить снайперов."
+		<< "4. Добавить артилерию."
+		<< "5. Назад." << endl;
+	
 }
